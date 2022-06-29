d70fff891b4236134211adc9974d35ab4f56a23f
stevejr23 <stevej4r412@hotmail.com>
2022-06-28 20:27:34 -0500

    grupal5

--- taller/taller11/administrativo/urls.py
+++ taller/taller11/administrativo/urls.py
@@ -10,12 +10,12 @@ from . import views
 urlpatterns = [
         path('', views.index, name='index'),
         path('edificio/<int:id>', views.obtener_edificio, 
-            name='obtener_edficio'),
-        path('crear/edificio', views.crear_edficio, 
+            name='obtener_edificio'),
+        path('crear/edificio', views.crear_edificio, 
             name='crear_edificio'),
         path('editar_edificio/<int:id>', views.editar_edificio, 
             name='editar_edificio'),
-        path('eliminar/edficio/<int:id>', views.eliminar_edficio, 
+        path('eliminar/edificio/<int:id>', views.eliminar_edificio, 
             name='eliminar_edificio'),
         # Edificios
         path('crear/departamento', views.crear_departamento, 
@@ -24,5 +24,5 @@ urlpatterns = [
             name='editar_departamento'),
         path('crear/departamento/edificio/<int:id>', 
             views.crear_departamento_edificio, 
-            name='crear_departamento_estudiante'),
+            name='crear_departamento_edificio'),
  ]
